cede9794a03925d44960ea9e5be1f067d1b94b74
Muniraju <muniraju.a@lge.com>
2022-07-07 15:54:29 +0530

    Migrate RP apps to use React18
    :Release Notes:
    Migrate RP apps to use React18
    
    :Detailed Notes:
    Migrate RP apps to use React18
    
    :Testing Performed:
    Verification pending
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [WRO-7546] Migrate RP apps to use React18
    
    Change-Id: Iad1349e0771183d601f6a26ae3e01ae16aae13f2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import {render} from 'react-dom';
+/* global ENACT_PACK_ISOMORPHIC */
+import {createRoot, hydrateRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
 import App from './App';
 import configureStore from './store';
-
+console.log('Enact upgrade..');
 const store = configureStore();
 const appElement = (<Provider store={store}><App /></Provider>);
-// In a browser environment, render instead of exporting
+
+// In a browser environment, render the app to the document.
 if (typeof window !== 'undefined') {
-	render(appElement, document.getElementById('root'));
+    if (ENACT_PACK_ISOMORPHIC) {
+        hydrateRoot(document.getElementById('root'), appElement);
+    } else {
+        createRoot(document.getElementById('root')).render(appElement);
+    }
 }
 
 export default appElement;
